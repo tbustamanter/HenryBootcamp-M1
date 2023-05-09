7fa7c92a0d2431f143fecb19da7382d3ab17068c
tbustamar <theina87@gmal.com>
2023-05-08 18:06:50 -0700

    homework 1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,29 +1,31 @@
 "use strict";
 
 function BinarioADecimal(num) {
-  // 011001 = 25
-  // <------- leyendo de der a izq
-  //    1 * 2 ** 0  = 1
-  //    0 * 2 ** 1  = 0
-  //    0 * 2 ** 2  = 0
-  //    1 * 2 ** 3  =  8
-  //    1 * 2 ** 4  =  16
-  //    0 * 2 ** 5  =   0
+  let array = num.split('').reverse();
+  let decimal = 0;
+  for(let x=0; x <= array.length; x++){
+    if(array[x] == 1){
+    decimal = decimal + Math.pow(2, x);
+    }
+  }
+  return decimal;
+
 }
 
 function DecimalABinario(num) {
-  // 91 = 1011011
-  // 91 / 2 = 45  (.5) ===> 1
-  // 45 / 2 = 22   (.5) ===> 1
-  // 22 / 2 = 11  (0) ===> 0
-  // 91 / 2 = 45 residuo 1
-  // 45 / 2 = 22 residuo 1
-  // 22 / 2 = 11 residuo 0
-  // 11/ 2 = 5 residuo 1
-  // 5 / 2 = 2 residuo 1
-  // 2 / 2 = 1 residuo 0
-  // 1 / 2 = 0 residuo 1
+  let binario = [];
+  while(num > 0){
+   
+    if(num%2 > 0){
+      binario.push(1);
+    }else{
+      binario.push(0);
+    }
+    num = Math.floor(num/2); 
+  }
+    return binario.reverse().join('');
 }
+
 
 module.exports = {
   BinarioADecimal,
